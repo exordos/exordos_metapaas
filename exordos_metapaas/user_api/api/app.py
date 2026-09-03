0466efd6bba1a270f4a2824a04e2118ea2f58973
slashburygin <slashburygin@gmail.com>
2026-09-03 18:40:38 +0300

    fix: remove empty OpenAPI components

--- exordos_metapaas/user_api/api/app.py
+++ exordos_metapaas/user_api/api/app.py
@@ -58,7 +58,6 @@ def get_openapi_engine():
             description=(f"OpenAPI - Exordos MetaPaaS {versions.API_VERSION_1_0}"),
         ),
         paths=openapi_structures.OpenApiPaths(),
-        components=openapi_structures.OpenApiComponents(),
     )
     return openapi_engine
 
